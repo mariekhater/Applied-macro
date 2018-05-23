5ecd41185c82d60e66a36c78b7e9738066c9dd15
mariekhater <36771771+mariekhater@users.noreply.github.com>
2018-05-23 21:47:58 +0200

    Add files via upload

--- SVAR3.R
+++ SVAR3.R
@@ -70,7 +70,13 @@ adfTest(TAlog-Glog, type="c")
 adfTest(TAlog-Glog, type="ct")
 #On ne peut pas rejeter pour les deux tests que c'est non stationnaire
 #BG trouve de meme (en ce qui concerne la relation de cointégration) pour la France sur des données antérieures et idem USA avec BP(2002)
+
 #Test de Johansen
+
+deltaPIB <- diff(PIBlog)*100
+deltaTA <- diff(TAlog)*100
+deltaG <- diff(Glog)*100
+
 X <- data.frame(deltaTA, deltaG, deltaPIB)
 jotest=ca.jo(X, type="trace", K=3, ecdet="const", spec="longrun")
 summary(jotest)
@@ -91,11 +97,6 @@ summary(jotest2)
 #On prend tout en différence première *100 pour avoir en pourcentage : argument Hamilton (1994)
 #le passage en différence accroît la précision de l'estimation du modèle,
 #dans une situation où l'échantillon est de petite taille
-
-deltaPIB <- diff(PIBlog)*100
-deltaTA <- diff(TAlog)*100
-deltaG <- diff(Glog)*100
-
 plot(deltaPIB)
 plot(deltaTA)
 plot(deltaG)
@@ -210,18 +211,20 @@ M1[3,2] <- - summary(lineareq3)$coefficients[2,1]
 #Tous les coefficients de M1 sont aussi identifiés
 
 #Les IRFs
-
+horizon <- 30
+n_iter <- 100
 
 P <- inv(M1)%*%M2
 
-results <- matrix(numeric(0), 3,10)
+results <- matrix(numeric(0), 3,horizon)
+resultsbis <- matrix(numeric(0), 3, horizon)
 
 #On construit la matrix rho pour pouvoir après calculer Xt=rho*X(t-1)+epsilont
 rho <- matrix(numeric(0),3,3)
 
 for (j in 1:3)
 {
-    rho[1,j] <- coef(estres)$deltaTAres[j,1]
+  rho[1,j] <- coef(estres)$deltaTAres[j,1]
 }
 
 for (j in 1:3)
@@ -234,6 +237,7 @@ for (j in 1:3)
   rho[3,j] <- coef(estres)$deltaPIBres[j,1]
 }
 
+
 epsilon <- matrix(numeric(0), 3,1)
 epsilon[1,1]<-1
 epsilon[2,1]<-0
@@ -241,115 +245,342 @@ epsilon[3,1]<-0
 
 results[1:3,1] <- P%*%epsilon
 
-for (j in 2:10)
+#normalisation à 1 euro
+epsilonbis <- matrix(numeric(0), 3,1)
+epsilonbis[1,1]<-1/results[1,1]
+epsilonbis[2,1]<-0
+epsilonbis[3,1]<-0
+
+resultsbis[1:3,1] <- P%*%epsilonbis
+
+for (j in 2:horizon)
 {
-  results[,j] <- rho%*%results[,j-1]
+  resultsbis[,j] <- rho%*%resultsbis[,j-1]
 }
+
+#calcul en niveau
+resultsNiveauxLn<-matrix(numeric(0), 3,horizon)
+resultsNiveauxLn[,1]<-resultsbis[,1]
+
+for (j in 2:horizon)
+{
+  resultsNiveauxLn[,j]<-resultsbis[,j]+resultsNiveauxLn[,j-1]
+}
+
+
 
 #Monte Carlo
-IRF_TA <- matrix(numeric(0), 10,5)
-IRF_G <- matrix(numeric(0), 10,5)
-IRF_PIB <- matrix(numeric(0),10,5)
-for (i in 1:5)
+IRF_TA <- matrix(numeric(0), horizon,n_iter)
+IRF_G <- matrix(numeric(0), horizon,n_iter)
+IRF_PIB <- matrix(numeric(0),horizon,n_iter)
+for (i in 1:n_iter)
 {
-    #Générer de nouvelles données de meme taille
-    Xmonte <- matrix(numeric(0), 3,115)
-    Xmonte[,1]=P%*%residuals(estres)[sample(1:115, 1),]
-    for (j in 2:115)
-    {
-      Xmonte[,j]=rho%*%Xmonte[,j-1]+P%*%residuals(estres)[sample(1:115, 1),]
-    }
-    
-    #Estime le modèle sur ces données
-          
-    Xmonte <- data.frame(Xmonte[1,], Xmonte[2,], Xmonte[3,])
-    estmonte <- VAR(Xmonte,p=1, type="cons")
-    estmonte_residuals <-residuals(estmonte)
-      #Estimer le nouveau P (donc M1 et M2)
-          
-      Mmonte1 = matrix( c(1, 0, -99, 0, 1, -99,-99,-99, 1), nrow=3, ncol=3) #je mets 99 quand c'est une valeur inconnue pour le moment
-      Mmonte2 = matrix( c(1, 99, 0, 99, 1, 0,0,0, 1), nrow=3, ncol=3)
-      Mmonte1[1,3]<- -0.8
-      Mmonte1[2,3]<-0
-      Mmonte2[1,2]<- 0
-          
-          #On calcule les epsilon(ta) 
-          epsmonte_ta=estmonte_residuals[,1]-0.8*estmonte_residuals[,3]
-          
-          #Regression MCO sur la deuxième equation pour obtenir beta_gt
-          linearmonteeq2 <- lm(estmonte_residuals[,2] ~ epsmonte_ta - 1) 
-          epsmonte_tg <- residuals(linearmonteeq2)
-          Mmonte2[2,1]<- summary(linearmonteeq2)$coefficients[1, 1]
-          
-          
-          #Regression MCO (avec variables instrumentales) sur la troisième équation pour obtenir gamma_yt et gamma_yg
-          #Les IV sont eps_ta pour u_ta et eps_tg pour u_tg
-          linearmonteeq3 <- ivreg(estmonte_residuals[,3] ~ estmonte_residuals[,1] + estmonte_residuals[,2] -1| epsmonte_ta +epsmonte_tg)
-          Mmonte1[3,1]<- - summary(linearmonteeq3)$coefficients[1,1]
-          Mmonte1[3,2] <- - summary(linearmonteeq3)$coefficients[2,1]
-          #Tous les coefficients de M1 sont aussi identifiés
-          
-      #IRFs
-      Pmonte <- inv(Mmonte1)%*%Mmonte2
-          
-      resultsmonte <- matrix(numeric(0), 3,10)
-          
-          #On construit la matrix rho pour pouvoir après calculer Xt=rho*X(t-1)+epsilont
-          rhomonte <- matrix(numeric(0),3,3)
-          
-          for (j in 1:3)
-          {
-            rhomonte[1,j] <- coef(estmonte)$Xmonte.1...[j,1]
-          }
-          
-          for (j in 1:3)
-          {
-            rhomonte[2,j] <- coef(estmonte)$Xmonte.2...[j,1]
-          }
-          
-          for (j in 1:3)
-          {
-            rhomonte[3,j] <- coef(estmonte)$Xmonte.3...[j,1]
-          }
-          
-      epsilonmonte <- matrix(numeric(0), 3,1)
-      epsilonmonte[1,1]<-1
-      epsilonmonte[2,1]<-0
-      epsilonmonte[3,1]<-0
-          
-      resultsmonte[,1] <- Pmonte%*%epsilonmonte
-          
-      for (j in 2:10)
-        {
-          resultsmonte[,j] <- rhomonte%*%resultsmonte[,j-1]
-      }
-      
-  IRF_TA[,i] <-resultsmonte[1,]
-  IRF_G[,i] <- resultsmonte[2,]
-  IRF_PIB[,i]<- resultsmonte[3,]
+  #Générer de nouvelles donn?es de meme taille
+  Xmonte <- matrix(numeric(0), 3,115)
+  Xmonte[,1]=P%*%residuals(estres)[sample(1:115, 1),]
+  for (j in 2:115)
+  {
+    Xmonte[,j]=rho%*%Xmonte[,j-1]+P%*%residuals(estres)[sample(1:115, 1),]
+  }
+  #Estime le modèle sur ces données
+  
+  Xmonte <- data.frame(Xmonte[1,], Xmonte[2,], Xmonte[3,])
+  estmonte <- VAR(Xmonte,p=1, type="cons")
+  estmonte_residuals <-residuals(estmonte)
+  #Estimer le nouveau P (donc M1 et M2)
+  
+  Mmonte1 = matrix( c(1, 0, -99, 0, 1, -99,-99,-99, 1), nrow=3, ncol=3) #je mets 99 quand c'est une valeur inconnue pour le moment
+  Mmonte2 = matrix( c(1, 99, 0, 99, 1, 0,0,0, 1), nrow=3, ncol=3)
+  Mmonte1[1,3]<- -0.8
+  Mmonte1[2,3]<-0
+  Mmonte2[1,2]<- 0
+  
+  #On calcule les epsilon(ta) 
+  epsmonte_ta=estmonte_residuals[,1]-0.8*estmonte_residuals[,3]
+  
+  #Regression MCO sur la deuxième equation pour obtenir beta_gt
+  linearmonteeq2 <- lm(estmonte_residuals[,2] ~ epsmonte_ta - 1) 
+  epsmonte_tg <- residuals(linearmonteeq2)
+  Mmonte2[2,1]<- summary(linearmonteeq2)$coefficients[1, 1]
+  
+  
+  #Regression MCO (avec variables instrumentales) sur la troisième équation pour obtenir gamma_yt et gamma_yg
+  #Les IV sont eps_ta pour u_ta et eps_tg pour u_tg
+  linearmonteeq3 <- ivreg(estmonte_residuals[,3] ~ estmonte_residuals[,1] + estmonte_residuals[,2] -1| epsmonte_ta +epsmonte_tg)
+  Mmonte1[3,1]<- - summary(linearmonteeq3)$coefficients[1,1]
+  Mmonte1[3,2] <- - summary(linearmonteeq3)$coefficients[2,1]
+  #Tous les coefficients de M1 sont aussi identifi?s
+  
+  #IRFs
+  Pmonte <- inv(Mmonte1)%*%Mmonte2
+  
+  resultsmonte <- matrix(numeric(0), 3,horizon)
+  resultsmontebis <- matrix(numeric(0), 3,horizon)
+  resultsmonteNiveauxLn <- matrix(numeric(0), 3,horizon)
+  
+  #On construit la matrix rho pour pouvoir après calculer Xt=rho*X(t-1)+epsilont
+  rhomonte <- matrix(numeric(0),3,3)
+  
+  for (j in 1:3)
+  {
+    rhomonte[1,j] <- coef(estmonte)$Xmonte.1...[j,1]
+  }
+  
+  for (j in 1:3)
+  {
+    rhomonte[2,j] <- coef(estmonte)$Xmonte.2...[j,1]
+  }
+  
+  for (j in 1:3)
+  {
+    rhomonte[3,j] <- coef(estmonte)$Xmonte.3...[j,1]
+  }
+  
+  epsilonmonte <- matrix(numeric(0), 3,1)
+  epsilonmonte[1,1]<-1
+  epsilonmonte[2,1]<-0
+  epsilonmonte[3,1]<-0
+  
+  resultsmonte[,1] <- Pmonte%*%epsilonmonte
+  
+  epsilonmontebis <- matrix(numeric(0), 3,1)
+  epsilonmontebis[1,1]<-1/(resultsmonte[1,1])
+  epsilonmontebis[2,1]<-0
+  epsilonmontebis[3,1]<-0
+  
+  resultsmontebis[,1] <- Pmonte%*%epsilonmontebis
+  
+  for (j in 2:horizon)
+  {
+    resultsmontebis[,j] <- rhomonte%*%resultsmontebis[,j-1]
+  }
+  
+  resultsmonteNiveauxLn[,1]<-resultsmontebis[,1]
+  
+  for (j in 2:horizon)
+  {
+    resultsmonteNiveauxLn[,j]<-resultsmontebis[,j]+resultsmonteNiveauxLn[,j-1]
+  }
+  
+  
+  IRF_TA[,i] <-resultsmonteNiveauxLn[1,]
+  IRF_G[,i] <- resultsmonteNiveauxLn[2,]
+  IRF_PIB[,i]<- resultsmonteNiveauxLn[3,]
+  
+  
 }
 
-varIRF_TA <- matrix(numeric(0), 10,1)
-for (j in 1:10)
+varIRF_TA <- matrix(numeric(0), horizon,1)
+for (j in 1:horizon)
 {
   varIRF_TA[j,]<-var(IRF_TA[j,])
 }
 sc=1.6449 #90% intervalle de confiance
-Max_TA <- results[1,] +sc*sqrt(varIRF_TA)
-Min_TA <- results[1,] -sc*sqrt(varIRF_TA)
+Max_TA <- resultsNiveauxLn[1,] +sc*sqrt(varIRF_TA)
+Min_TA <- resultsNiveauxLn[1,] -sc*sqrt(varIRF_TA)
 
-varIRF_G <- matrix(numeric(0), 10,1)
-for (j in 1:10)
+
+varIRF_G <- matrix(numeric(0), horizon,1)
+for (j in 1:horizon)
 {
   varIRF_G[j,]<-var(IRF_G[j,])
 }
-Max_G <- results[2,] +sc*sqrt(varIRF_G)
-Min_G <- results[2,] -sc*sqrt(varIRF_G)
+Max_G <- resultsNiveauxLn[2,] +sc*sqrt(varIRF_G)
+Min_G <- resultsNiveauxLn[2,] -sc*sqrt(varIRF_G)
 
-varIRF_PIB <- matrix(numeric(0), 10,1)
-for (j in 1:10)
+varIRF_PIB <- matrix(numeric(0), horizon,1)
+for (j in 1:horizon)
 {
   varIRF_PIB[j,]<-var(IRF_PIB[j,])
 }
-Max_PIB <- results[2,] +sc*sqrt(varIRF_PIB)
-Min_PIB <- results[2,] -sc*sqrt(varIRF_PIB)
+Max_PIB <- resultsNiveauxLn[3,] +sc*sqrt(varIRF_PIB)
+Min_PIB <- resultsNiveauxLn[3,] -sc*sqrt(varIRF_PIB)
+
+plot(resultsNiveauxLn[3,],type="l",col="blue",ylim=c(-1,1))
+lines(Max_PIB,type="l",col="red")
+lines(Min_PIB,type="l",col="red")
+
+plot(resultsNiveauxLn[2,],type="l",col="blue",ylim=c(-1,1))
+lines(Max_G,type="l",col="red")
+lines(Min_G,type="l",col="red")
+
+plot(resultsNiveauxLn[1,],type="l",col="blue",ylim=c(0,2))
+lines(Max_TA,type="l",col="red")
+lines(Min_TA,type="l",col="red")
+
+
+#Choc sur dépenses
+results2<-matrix(numeric(0),3,horizon)
+results2bis<-matrix(numeric(0),3,horizon)
+
+epsilon2 <- matrix(numeric(0), 3,1)
+epsilon2bis <- matrix(numeric(0), 3,1)
+
+
+epsilon2[1,1]<-0
+epsilon2[2,1]<-1
+epsilon2[3,1]<-0
+
+results2[1:3,1] <- P%*%epsilon2
+
+
+epsilon2bis[1,1]<-0
+epsilon2bis[2,1]<-1/results2[2,1]
+epsilon2bis[3,1]<-0
+
+results2bis[1:3,1] <- P%*%epsilon2bis
+
+for (j in 2:horizon)
+{
+  results2bis[,j] <- rho%*%results2bis[,j-1]
+}
+
+results2NiveauxLn<-matrix(numeric(0), 3,horizon)
+results2NiveauxLn[,1]<-results2bis[,1]
+
+for (j in 2:horizon)
+{
+  results2NiveauxLn[,j]<-results2bis[,j]+results2NiveauxLn[,j-1]
+}
+
+
+#Monte Carlo
+IRF_TA_2 <- matrix(numeric(0), horizon,n_iter)
+IRF_G_2 <- matrix(numeric(0), horizon,n_iter)
+IRF_PIB_2 <- matrix(numeric(0),horizon,n_iter)
+for (i in 1:n_iter)
+{
+  #Générer de nouvelles données de meme taille
+  Xmonte_2 <- matrix(numeric(0), 3,115)
+  Xmonte_2[,1]=P%*%residuals(estres)[sample(1:115, 1),]
+  for (j in 2:115)
+  {
+    Xmonte_2[,j]=rho%*%Xmonte_2[,j-1]+P%*%residuals(estres)[sample(1:115, 1),]
+  }
+  
+  #Estime le modèle sur ces données
+  
+  Xmonte_2 <- data.frame(Xmonte_2[1,], Xmonte_2[2,], Xmonte_2[3,])
+  estmonte_2 <- VAR(Xmonte_2,p=1, type="cons")
+  estmonte_residuals_2 <-residuals(estmonte_2)
+  #Estimer le nouveau P (donc M1 et M2)
+  
+  Mmonte1_2 = matrix( c(1, 0, -99, 0, 1, -99,-99,-99, 1), nrow=3, ncol=3) #je mets 99 quand c'est une valeur inconnue pour le moment
+  Mmonte2_2 = matrix( c(1, 99, 0, 99, 1, 0,0,0, 1), nrow=3, ncol=3)
+  Mmonte1_2[1,3]<- -0.8
+  Mmonte1_2[2,3]<-0
+  Mmonte2_2[1,2]<- 0
+  
+  #On calcule les epsilon(ta) 
+  epsmonte_ta_2=estmonte_residuals_2[,1]-0.8*estmonte_residuals_2[,3]
+  
+  #Regression MCO sur la deuxi?me equation pour obtenir beta_gt
+  linearmonteeq2_2 <- lm(estmonte_residuals_2[,2] ~ epsmonte_ta_2 - 1) 
+  epsmonte_tg_2 <- residuals(linearmonteeq2_2)
+  Mmonte2_2[2,1]<- summary(linearmonteeq2_2)$coefficients[1, 1]
+  
+  
+  #Regression MCO (avec variables instrumentales) sur la troisième équation pour obtenir gamma_yt et gamma_yg
+  #Les IV sont eps_ta pour u_ta et eps_tg pour u_tg
+  linearmonteeq3_2 <- ivreg(estmonte_residuals_2[,3] ~ estmonte_residuals_2[,1] + estmonte_residuals_2[,2] -1| epsmonte_ta_2 +epsmonte_tg_2)
+  Mmonte1_2[3,1]<- - summary(linearmonteeq3_2)$coefficients[1,1]
+  Mmonte1_2[3,2] <- - summary(linearmonteeq3_2)$coefficients[2,1]
+  #Tous les coefficients de M1 sont aussi identifi?s
+  
+  #IRFs
+  Pmonte_2 <- inv(Mmonte1_2)%*%Mmonte2_2
+  
+  resultsmonte_2 <- matrix(numeric(0), 3,horizon)
+  resultsmontebis_2 <- matrix(numeric(0), 3,horizon)
+  resultsmonteNiveauxLn_2 <- matrix(numeric(0), 3,horizon)
+  
+  #On construit la matrix rho pour pouvoir apr?s calculer Xt=rho*X(t-1)+epsilont
+  rhomonte_2 <- matrix(numeric(0),3,3)
+  
+  for (j in 1:3)
+  {
+    rhomonte_2[1,j] <- coef(estmonte_2)$Xmonte_2.1...[j,1]
+  }
+  
+  for (j in 1:3)
+  {
+    rhomonte_2[2,j] <- coef(estmonte_2)$Xmonte_2.2...[j,1]
+  }
+  
+  for (j in 1:3)
+  {
+    rhomonte_2[3,j] <- coef(estmonte_2)$Xmonte_2.3...[j,1]
+  }
+  
+  epsilonmonte_2 <- matrix(numeric(0), 3,1)
+  epsilonmonte_2[1,1]<-0
+  epsilonmonte_2[2,1]<-1
+  epsilonmonte_2[3,1]<-0
+  
+  resultsmonte_2[,1] <- Pmonte_2%*%epsilonmonte_2
+  
+  epsilonmontebis_2 <- matrix(numeric(0), 3,1)
+  epsilonmontebis_2[1,1]<-0
+  epsilonmontebis_2[2,1]<-1/(resultsmonte_2[2,1])
+  epsilonmontebis_2[3,1]<-0
+  
+  resultsmontebis_2[,1] <- Pmonte_2%*%epsilonmontebis_2
+  
+  for (j in 2:horizon)
+  {
+    resultsmontebis_2[,j] <- rhomonte_2%*%resultsmontebis_2[,j-1]
+  }
+  
+  resultsmonteNiveauxLn_2[,1]<-resultsmontebis_2[,1]
+  
+  for (j in 2:horizon)
+  {
+    resultsmonteNiveauxLn_2[,j]<-resultsmontebis_2[,j]+resultsmonteNiveauxLn_2[,j-1]
+  }
+  
+  
+  IRF_TA_2[,i] <-resultsmonteNiveauxLn_2[1,]
+  IRF_G_2[,i] <- resultsmonteNiveauxLn_2[2,]
+  IRF_PIB_2[,i]<- resultsmonteNiveauxLn_2[3,]
+  
+  
+}
+
+varIRF_TA_2 <- matrix(numeric(0), horizon,1)
+for (j in 1:horizon)
+{
+  varIRF_TA_2[j,]<-var(IRF_TA_2[j,])
+}
+sc=1.6449 #90% intervalle de confiance
+Max_TA_2 <- results2NiveauxLn[1,] +sc*sqrt(varIRF_TA_2)
+Min_TA_2 <- results2NiveauxLn[1,] -sc*sqrt(varIRF_TA_2)
+
+
+varIRF_G_2 <- matrix(numeric(0), horizon,1)
+for (j in 1:horizon)
+{
+  varIRF_G_2[j,]<-var(IRF_G_2[j,])
+}
+Max_G_2 <- results2NiveauxLn[2,] +sc*sqrt(varIRF_G_2)
+Min_G_2 <- results2NiveauxLn[2,] -sc*sqrt(varIRF_G_2)
+
+varIRF_PIB_2 <- matrix(numeric(0), horizon,1)
+for (j in 1:horizon)
+{
+  varIRF_PIB_2[j,]<-var(IRF_PIB_2[j,])
+}
+Max_PIB_2 <- results2NiveauxLn[3,] +sc*sqrt(varIRF_PIB_2)
+Min_PIB_2 <- results2NiveauxLn[3,] -sc*sqrt(varIRF_PIB_2)
+
+plot(results2NiveauxLn[3,],type="l",col="blue",ylim=c(-2,2))
+lines(Max_PIB_2,type="l",col="red")
+lines(Min_PIB_2,type="l",col="red")
+
+plot(results2NiveauxLn[2,],type="l",col="blue",ylim=c(0,4))
+lines(Max_G_2,type="l",col="red")
+lines(Min_G_2,type="l",col="red")
+
+plot(results2NiveauxLn[1,],type="l",col="blue",ylim=c(-5,5))
+lines(Max_TA_2,type="l",col="red")
+lines(Min_TA_2,type="l",col="red")
+
